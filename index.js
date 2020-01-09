fd61578433997ec5fbb7c60f598b62d9b03d65ba
Petros Koulianos <petranb2@gmail.com>
2020-01-09 15:01:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-
 const express = require('express')
-const { spawn } = require('child_process');
+const {
+	spawn
+} = require('child_process');
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
+	var dataToSend;
 	const pyProg = spawn('python', ['script.py']);
-	pyProg.stdout.on('data', function(data) {
-		console.log(data.toString());
-		res.send(data.toString())
+	pyProg.stdout.on('data', function (data) {
+		console.log('Streaming data from python script ...');
+		dataToSend = data.toString();
 	});
-	
+
+	pyProg.on('exit', (code) => {
+		console.log(`Child exited with code ${code}`);
+		res.send(dataToSend)
+	});
+
 })
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
