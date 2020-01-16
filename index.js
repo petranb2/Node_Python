bdffb0dec5e611e10f3f0d0245009b79d63b30fc
Petros Koulianos <petranb2@gmail.com>
2020-01-16 18:12:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express')
-const {
-	spawn
-} = require('child_process');
+const {spawn} = require('child_process');
 const app = express()
 const port = 3000
+
 app.get('/', (req, res) => {
-	var dataToSend;
-	const pyProg = spawn('python', ['script.py']);
-	pyProg.stdout.on('data', function (data) {
+	
+	var dataToSend ;
+	var largeDataSet = [];
+	// spawn new child process to call the python script
+	const python = spawn('python', ['script3.py']);
+	// collect data from script
+	python.stdout.on('data', function (data) {
 		console.log('Pipe data from python script ...');
-		dataToSend = data.toString();
+		//dataToSend =  data;
+		largeDataSet.push(data);
 	});
-
-	pyProg.on('close', (code) => {
-		console.log(`Child closed with code ${code}`);
-		res.send(dataToSend)
+	// in close event we are sure that stream is from child process is closed
+	python.on('close', (code) => {
+	console.log(`child process close all stdio with code ${code}`);
+	// send data to browser
+	res.send(largeDataSet.join(""))
 	});
-	pyProg.on('exit', (code) => {
-		console.log(`Child exited with code ${code}`);
-	});
-
+	
 })
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
