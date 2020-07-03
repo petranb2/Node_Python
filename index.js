c8872219b7ee27deaa1dd875aea561f765a117ae
Nafis Fuad <fuadbd14@gmail.com>
2020-07-03 09:51:04 +0600

    validate json, minor changes

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express')
-const {spawn} = require('child_process');
+const { spawn } = require('child_process')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-	
-	var dataToSend ;
-	var largeDataSet = [];
-	// spawn new child process to call the python script
-	const python = spawn('python', ['script3.py']);
-	// collect data from script
-	python.stdout.on('data', function (data) {
-		console.log('Pipe data from python script ...');
-		//dataToSend =  data;
-		largeDataSet.push(data);
-	});
-	// in close event we are sure that stream is from child process is closed
-	python.on('close', (code) => {
-	console.log(`child process close all stdio with code ${code}`);
-	// send data to browser
-	res.send(largeDataSet.join(""))
-	});
-	
+  let dataToSend
+  let largeDataSet = []
+  // spawn new child process to call the python script
+  const python = spawn('python', ['script3.py'])
+
+  // collect data from script
+  python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...')
+    //dataToSend =  data;
+    largeDataSet.push(data)
+  })
+
+  // in close event we are sure that stream is from child process is closed
+  python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`)
+    // send data to browser
+    res.send(largeDataSet.join(''))
+  })
 })
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
